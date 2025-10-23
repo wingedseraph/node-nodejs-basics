5f66fdfc0624dbd3f197158c99e85abe4750250a
wingedseraph <r89647@gmail.com>
2025-10-24 00:20:47 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { URL, fileURLToPath } from "node:url";
+
+import { ERRORS } from "../const.js";
+
+const filePath = fileURLToPath(
+  new URL("./files/fileToCalculateHashFor.txt", import.meta.url)
+);
+
 const calculateHash = async () => {
-  // Write your code here
+  try {
+    const fileContent = createReadStream(filePath);
+    const hash = createHash("sha256");
+
+    for await (const part of fileContent) {
+      hash.update(part);
+    }
+    const hexHash = hash.digest("hex");
+
+    console.log(hexHash);
+  } catch {
+    throw new Error(ERRORS.FS_OPERATION_FAILED);
+  }
 };
 
 await calculateHash();
