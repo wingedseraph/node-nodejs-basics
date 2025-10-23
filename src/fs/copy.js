be4690a6f258a4add11c6a663ffefae90e68892a
wingedseraph <r89647@gmail.com>
2025-10-23 16:27:34 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { cp } from "node:fs/promises";
+import { URL, fileURLToPath } from "node:url";
+
+import { ERRORS } from "../const.js";
+import { isFileExists } from "../utils.js";
+
+const isFileExists = fileURLToPath(new URL("./files/", import.meta.url));
+const filePathDestination = fileURLToPath(
+  new URL("./files_copy/", import.meta.url)
+);
+
 const copy = async () => {
-  // Write your code here
+  if (!isFileExists(isFileExists) || isFileExists(filePathDestination)) {
+    throw new Error(ERRORS.FS_OPERATION_FAILED);
+  }
+
+  await cp(isFileExists, filePathDestination, { recursive: true });
 };
 
 await copy();
