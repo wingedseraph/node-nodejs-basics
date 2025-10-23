c5fde90f9cad084b6a632cadd749d18531602219
wingedseraph <r89647@gmail.com>
2025-10-23 22:36:58 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { rename as rename_node } from "node:fs/promises";
+import { URL, fileURLToPath } from "node:url";
+
+import { isFileExists } from "../utils.js";
+import { ERRORS } from "../const.js";
+
+const oldPath = fileURLToPath(
+  new URL("./files/wrongFilename.txt", import.meta.url)
+);
+const newPath = fileURLToPath(
+  new URL("./files/properFilename.md", import.meta.url)
+);
+
 const rename = async () => {
-  // Write your code here
+  const isMarkdownFileExists = await isFileExists(newPath);
+
+  if (!isMarkdownFileExists) {
+    rename_node(oldPath, newPath).catch(() => {
+      throw new Error(ERRORS.FS_OPERATION_FAILED);
+    });
+  } else {
+    throw new Error(ERRORS.FS_OPERATION_FAILED);
+  }
 };
 
 await rename();
