c5e5d82ef428a33dffbca2f93ca10de4b232082b
wingedseraph <r89647@gmail.com>
2025-10-25 01:25:24 +0300

    refactor: add to const CP error message

--- src/const.js
+++ src/const.js
@@ -1,4 +1,5 @@
 export const ERRORS = {
   FS_OPERATION_FAILED: "FS operation failed",
+  CP_OPERATION_FAILED: "CP operation failed",
   STREAM_OPERATION_FAILED: "STREAM operation failed",
 };
--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,13 +1,13 @@
 import { fork } from "node:child_process";
 import { URL, fileURLToPath } from "node:url";
+import { ERRORS } from "../const.js";
 
 const fileSource = fileURLToPath(new URL("./files/script.js", import.meta.url));
-const ERROR_MESSAGE = "child error";
 
 const spawnChildProcess = async (args) => {
   const child = fork(fileSource, args);
   child.on("error", () => {
-    throw new Error(ERROR_MESSAGE);
+    throw new Error(ERRORS.CP_OPERATION_FAILED);
   });
 };
 
