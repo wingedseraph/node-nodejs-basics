86e068baa71cb1eff5ca50fa19a1663c6174b486
wingedseraph <r89647@gmail.com>
2025-10-24 23:46:13 +0300

    feat: implement streams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import { URL, fileURLToPath } from "node:url";
+
+import { ERRORS } from "../const.js";
+
+const filePath = fileURLToPath(
+  new URL("./files/fileToRead.txt", import.meta.url)
+);
+
 const read = async () => {
-  // Write your code here
+  try {
+    const fileContent = createReadStream(filePath);
+
+    for await (const chunk of fileContent) {
+      stdout.write(chunk);
+    }
+  } catch {
+    throw new Error(ERRORS.FS_OPERATION_FAILED);
+  }
 };
 
 await read();
