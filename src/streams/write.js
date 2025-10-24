6a62f9983b99363224ba28374e4b37144769924b
wingedseraph <r89647@gmail.com>
2025-10-25 01:23:45 +0300

    feat: implement streams/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+import { URL, fileURLToPath } from "node:url";
+import { pipeline } from "node:stream/promises";
+
+import { ERRORS } from "../const.js";
+
+const filePath = fileURLToPath(
+  new URL("./files/fileToWrite.txt", import.meta.url)
+);
+
 const write = async () => {
-  // Write your code here
+  try {
+    await pipeline(stdin, createWriteStream(filePath));
+  } catch {
+    throw new Error(ERRORS.STREAM_OPERATION_FAILED);
+  }
 };
 
 await write();
