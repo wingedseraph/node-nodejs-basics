be9b002618271909d3e86a3ad21263d532581483
wingedseraph <r89647@gmail.com>
2025-10-25 15:57:39 +0300

    feat: implement zip/decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import process from "node:process";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream";
+
+import { getFilePath } from "../utils.js";
+
+const gzip = createUnzip();
+
+const filePathSource = getFilePath("./files/archive.gz");
+const filePathDestination = getFilePath("./files/fileToCompress.txt");
+
+const source = createReadStream(filePathSource);
+const destination = createWriteStream(filePathDestination);
+
 const decompress = async () => {
-  // Write your code here
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      process.exitCode = 1;
+      throw new Error(err.message);
+    }
+  });
 };
 
 await decompress();
