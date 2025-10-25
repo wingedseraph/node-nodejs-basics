98ce569c7d96d9c4c0b11e3d94e03bd2baca2acd
wingedseraph <r89647@gmail.com>
2025-10-25 18:45:35 +0300

    feat: implement wt/main&worker functions

--- src/const.js
+++ src/const.js
@@ -2,4 +2,5 @@ export const ERRORS = {
   FS_OPERATION_FAILED: "FS operation failed",
   CP_OPERATION_FAILED: "CP operation failed",
   STREAM_OPERATION_FAILED: "STREAM operation failed",
+  WORKER_OPERATION_FAILED: "WORKER: must be run on the main thread",
 };
