c35e2f0d14c31d57e471ae365782fac318ba21b8
wingedseraph <r89647@gmail.com>
2025-10-29 18:26:32 +0300

    refactor: group const with user message to Message group

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,7 @@
-export const INTRO = "Welcome to the File Manager, ";
-export const OUTRO_1 = "\nThank you for using File Manager, ";
-export const OUTRO_2 = ", goodbye!";
+export const MESSAGES = {
+  INTRO: "Welcome to the File Manager, ",
+  OUTRO_1: "\nThank you for using File Manager, ",
+  OUTRO_2: ", goodbye!",
+};
 
 export const FALLBACK_USERNAME = "guest";
